e0caef569b3b889fc0d24a031855409da5a37677
Joey Jirasevijinda <joey.jira@gmail.com>
2018-05-29 23:15:13 -0700

    FindMissingPositive with O(n) space

---
+++ Java/src/arrays/FindMissingPositive.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class FindMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+      int max = Integer.MIN_VALUE;
+      Map<Integer, Boolean> map = new HashMap<>();
+
+      for(int num : nums) {
+        map.put(num, true);
+        max = num > max ? num : max;
+      }
+
+      for(int i = 1; i <= max + 1; i++) {
+        if (map.get(i) == null) return i;
+      }
+        return -1;
+    }
+}
