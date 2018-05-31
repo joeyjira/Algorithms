62c465128c4d84c248cc05b44c62bfb3b86968aa
Joey Jirasevijinda <joey.jira@gmail.com>
2018-05-30 22:15:55 -0700

    wild card string matching

---
+++ Java/src/string/WildCardMatching.java
@@ -0,0 +1,16 @@
+package string;
+
+class WildCardMatching {
+    public boolean isMatch(String s, String p) {
+        char nextMatch;
+        char nextPattern;git
+        int j = 0;
+
+        for (int i = 0; i < p.length(); i++) {
+            nextMatch = s.charAt(j);
+            if (p.charAt(i) == '*') {
+                while ()
+            }
+        }
+    }
+}
