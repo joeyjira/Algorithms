d8d605099ac48309a0cb7fcd2a45fb4e79da9d7a
Joey Jirasevijinda <joey.jira@gmail.com>
2018-08-02 22:27:33 -0700

    better version of parity method

--- Primitive Types/Parity/Parity.java
+++ Primitive Types/Parity/Parity.java
@@ -1,5 +1,19 @@
 public class Parity {
   public static short bruteParity(long x) {
+    short result = 0;
+    while (x != 0) {
+      result ^= (x & 1);
+      x >>>= 1;
+    }
+    return result;
+  }
 
+  public static short betterParity(long x) {
+    short result = 0;
+    while (x != 0) {
+      result ^= 1;
+      x &= (x - 1); // Drops the lowest set bit of x
+    }
+    return result;
   }
 }
