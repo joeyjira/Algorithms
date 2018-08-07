7a46c5091bd718e3bc37bda6dd894faefb34da37
Joey Jirasevijinda <joey.jira@gmail.com>
2018-08-07 12:35:13 -0700

    move function for VillageState

--- Minis/robot.js
+++ Minis/robot.js
@@ -30,3 +30,15 @@ function VillageState(place, parcels) {
   this.place = place;
   this.parcels = parcels;
 }
+
+function.prototype.move = function(destination) {
+  if (!roadGraph[this.place].includes(destination)) {
+    return this;
+  } else {
+    let parcels = this.parcels.map(p => {
+      if (p.place != this.place) return p;
+      return {place: destination, address: p.address};
+    }).filter(p => p.place != p.address);
+    return new VillageState(destination, parcels);
+  }
+}
