3cadd993bf36ff30b9c20f91677effc019955205
Joey Jirasevijinda <joey.jira@gmail.com>
2018-08-14 12:07:22 -0700

    reverse integer method eg. 123 to 321

---
+++ LeetCode/Math/ReverseInteger.java
@@ -0,0 +1,17 @@
+public class ReverseInteger {
+  public int reverse(int x) {
+    int res = 0;
+
+    while (x != 0) {
+        int pop = x % 10;
+
+        if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+        if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+
+        res = res * 10 + pop;
+        x /= 10;
+    }
+
+    return res;
+  }
+}
